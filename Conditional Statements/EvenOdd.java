5b48fe76c0a84aa696e350b557fa14a4d70ede5e
Swaraj Phalke <swarajphalke555@gmail.com>
2025-05-11 23:33:24 +0530

    testing

--- Conditional Statements/EvenOdd.java
+++ Conditional Statements/EvenOdd.java
@@ -5,7 +5,6 @@ public class EvenOdd {
         Scanner sc = new Scanner(System.in);
         System.out.print( "Enter a number : 3");
 
-        
         int num = sc.nextInt();
         if(num%2==0){
             System.out.println("Number is Even");
