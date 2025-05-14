34dc4a30bc77900a0b815931e72f900a0348620c
Swaraj Phalke <swarajphalke555@gmail.com>
2025-05-14 20:04:26 +0530

    learning

--- Conditional Statements/IncomeTaxCalci.java
+++ Conditional Statements/IncomeTaxCalci.java
@@ -6,12 +6,13 @@ public class IncomeTaxCalci {
         System.out.print("Enter your Income : ");
         int income = sc.nextInt();
         int tax ;
+        
         if(income < 500000){
             tax = 0;
         }else if(income >= 500000 && income < 1000000){
             tax = (int) (income * 0.2);
         }else{
-            
+
             tax = (int) (income * 0.3);
         }
         System.out.println("Your tax is : "+ tax);
